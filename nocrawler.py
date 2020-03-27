23e134eadf9a6bbe616545ae6da09e8304db4f26
skybear308 <skybearok@outlook.com>
2020-03-27 16:34:55 -0700

    webdriver chrome set options

--- nocrawler.py
+++ nocrawler.py
@@ -1,12 +1,54 @@
 '''
+@ created_at: 2020-03-27
+@ created_by: BrightStar1120
+
+@ updated_at: 2020-03-27
+@ updated_by: BrightStar1120
 '''
+import pandas
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
+
+import random
 
 class NoCrawler(object):
     
     def __init__(self):
-        pass
+
+        self.chrome_opt = None
+        
+        self.proxy_list = list()
     
-    def get_proxy_list(self, proxy_list_path=None):
+    '''
+    @ description: set chrome options to use webdriver
+    @ params:
+    @ return: chrome_option
+    '''
+    def set_chrome_option(self):
+        user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+             'Chrome/80.0.3987.132 Safari/537.36'
+
+        chrome_opt = webdriver.ChromeOptions()
+        chrome_opt.add_argument('--no-sandbox')
+        chrome_opt.add_argument('--disable-dev-shm-usage')
+        chrome_opt.add_argument('--ignore-certificate-errors')
+        chrome_opt.add_argument("--disable-blink-features=AutomationControlled")
+        chrome_opt.add_argument(f'user-agent={user_agent}')
+        chrome_opt.headless = True
+
+        self.chrome_opt = chrome_opt
+        
+    '''
+    @ description: get proxys from proxy.txt file
+    @ params: proxy_list_path (str, default=None)
+    @ return boolean
+    '''
+    def get_proxy_list(self, proxy_list_path = None):
+        
         path = ""
         if proxy_list_path != None:
             path = proxy_list_path
@@ -17,3 +59,16 @@ class NoCrawler(object):
             return True
 
         return False
+    
+    '''
+    @ description: get random proxy
+    @ params:
+    @ return: proxy_ip
+    '''
+    def get_random_proxy(self):
+        
+        random_idx = random.randint(1, len(self.proxy_list) - 1)
+        proxy_ip = self.proxy_list[random_idx]
+        return proxy_ip
+    
+    
