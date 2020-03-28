e5102b6ef4d67f032491c2979cc5626ce534200a
skybear308 <skybearok@outlook.com>
2020-03-27 23:53:35 -0700

    crawl new vehicle from given url

--- nocrawler.py
+++ nocrawler.py
@@ -5,15 +5,14 @@
 @ updated_at: 2020-03-27
 @ updated_by: BrightStar1120
 '''
-import pandas
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.ui import WebDriverWait
 
+from selenium import webdriver
+
+import pandas
 import random
+import json
+import time
+import csv
 
 class NoCrawler(object):
     
@@ -22,6 +21,8 @@ class NoCrawler(object):
         self.chrome_opt = None
         
         self.proxy_list = list()
+        
+        self.output_path = 'results.csv'
     
     '''
     @ description: set chrome options to use webdriver
@@ -53,7 +54,7 @@ class NoCrawler(object):
         if proxy_list_path != None:
             path = proxy_list_path
         else:
-            path = "proxy.txt"
+            path = "proxies.txt"
         with open(path,"r") as file:
             self.proxy_list = file.readlines()    
             return True
@@ -71,4 +72,102 @@ class NoCrawler(object):
         proxy_ip = self.proxy_list[random_idx]
         return proxy_ip
     
+    '''
+    @ description: remove duplicated line of processed csv files
+    @ params:
+    @ return:
+    '''
+    def remove_duplicated_info(self):
+
+        # Remove any duplicated lines of processed csv (file):
+        df = pandas.read_csv(self.output_path)
+        df.drop_duplicates(subset=None, inplace=True)
+        df.to_csv(self.output_path, index=False)
     
+    '''
+    @ description:
+    @ params:
+    @ return:
+    '''
+    def append_header_to_csv(self):
+        
+        with open(self.output_path, 'a') as outfile:
+            
+            outfile.write('type,vin,price,year,make,model,trim')
+            outfile.write('\n')
+    
+    '''
+    @ description: main function to crawl
+    @ params:
+    @ return:
+    '''
+    def crawl_func(self):
+        
+        # add column header
+        self.append_header_to_csv()
+        
+        self.set_chrome_option()
+        
+        proxy = self.get_random_proxy()
+        
+        proxy_http = "http://" + proxy
+        
+        webdriver.DesiredCapabilities.CHROME['proxy'] = {
+            "httpProxy":proxy_http,
+            "ftpProxy":proxy_http,
+            "sslProxy":proxy_http,
+            "proxyType":"MANUAL",
+        }    
+        
+        with webdriver.Chrome(options = self.chrome_opt) as driver:
+            
+            driver.get("https://www.coxcdjr.com/new-vehicles/")
+            
+            columns = ['type', 'vin', 'price', 'year', 'make', 'model', 'trim']
+            
+            while True:
+                
+                # get the count of vehicles per page
+                vehicles_count_per_page = len(driver.find_elements_by_xpath('//a[@data-loc="results found"]'))
+                
+                for i in range(vehicles_count_per_page):
+                    
+                    script = 'return document.getElementsByClassName("stat-image-link")[' + str(i) + '].getAttribute("data-vehicle")'
+                    vehicle_info = driver.execute_script(script)
+                    
+                    vehicle_info_dict = json.loads(vehicle_info)
+                    
+                    vehicle_type = vehicle_info_dict['type']
+                    vehicle_vin = vehicle_info_dict['vin']
+                    vehicle_price = vehicle_info_dict['price']
+                    vehicle_year = vehicle_info_dict['year']
+                    vehicle_make = vehicle_info_dict['make']
+                    vehicle_model = vehicle_info_dict['model']
+                    vehicle_trim = vehicle_info_dict['trim']
+                    
+                    row = {'type': vehicle_type, 'vin': vehicle_vin, 'price': vehicle_price, 'year': vehicle_year, 'make': vehicle_make, 'model': vehicle_model, 'trim': vehicle_trim}
+                    
+                    with open(self.output_path, 'a', newline='') as outfile:
+                        writer = csv.DictWriter(outfile, fieldnames=columns)
+                        writer.writerow(row)
+                        
+                    
+                element = driver.find_element_by_xpath("//div[@id='results-header-pagination']//a[@data-testid='pagination-next-link']")
+                
+                
+                if bool(element.get_attribute("disabled")):
+                    break
+                
+                time.sleep(1)
+                
+                driver.execute_script("arguments[0].click();", element)
+                
+        self.remove_duplicated_info()        
+                
+nc = NoCrawler()
+
+# get list of proxys
+nc.get_proxy_list()
+
+# run main crawler function
+nc.crawl_func()
